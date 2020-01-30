64a9fd61d9f71faf7f09db39f33a7a71bb16021a
rus13 <rus13@users.noreply.github.com>
2020-01-30 11:55:32 +0100

    Update number_theory.cpp

--- number_theory.cpp
+++ number_theory.cpp
@@ -7,3 +7,16 @@ int gcd (int a, int b) {
 int lcm (int a, int b) {
     return a / gcd(a, b) * b;
 }
+
+int gcd(int a, int b, int & x, int & y) {
+    if (a == 0) {
+        x = 0;
+        y = 1;
+        return b;
+    }
+    int x1, y1;
+    int d = gcd(b % a, a, x1, y1);
+    x = y1 - (b / a) * x1;
+    y = x1;
+    return d;
+}
